541a9de9a01866082481178d6389a6b323693312
Vedant Raut <vdraut.2310@gmail.com>
2026-05-11 14:45:22 +0530

    Initial AutoTierX foundation setup

---
+++ build/CMakeFiles/autotierx.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/autotierx.dir/main.cpp.o"
+  "CMakeFiles/autotierx.dir/main.cpp.o.d"
+  "autotierx"
+  "autotierx.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/autotierx.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main() {
+    std::cout << "AutoTierX Started Successfully!" << std::endl;
+    return 0;
+}
